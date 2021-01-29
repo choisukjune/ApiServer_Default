b9bd1310fbd47230791d450f4a86b8f6a168cc3f
choisukjune <sukjune.choi@gmail.com>
2021-01-29 19:16:51 +0900

    .

--- js/insert.js
+++ js/insert.js
@@ -240,6 +240,75 @@ var paramToObject = function( url ){
 		res.end( r )	
 
 	});
+	/**
+	 * 쿼리파일을 실행하는 라우터
+	 * @function
+	 * @param {http.ClientRequest} req
+	 * <code>
+		{
+
+		}
+	* </code>
+	*
+	* @param {http.ClientResponse} res
+	* <code>
+		{
+
+		}
+	* </code>
+	*
+	* @example
+	* <code>
+		http://localhost:8888/insertBoard?data={...}
+	* </code>
+	*/
+	global.server.addRouter("/insertBoard",function( req, res ){
+		
+		var routerNm = req.url.split("?")[0];
+		var paramsO = paramToObject( req.url );//brandNm
+		var _tdbjs_nm = "insertBoard";
+		
+		res.statusCode = 200;
+		res.setHeader( "Access-Control-Allow-Headers", "Content-Type" );
+		res.setHeader( "Access-Control-Allow-Origin", "*" );
+		res.setHeader( "Access-Control-Allow-Methods", "OPTIONS,POST,GET" );
+		console.log( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ); 
+		
+		try
+		{
+			var _tQuery = fs.readFileSync( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ).toString();
+		}
+		catch( err )
+		{
+			console.log( routerNm + " - DBJS File Not Found! - " + err );
+			res.end("{ sucess : 0, data : null }");
+		}
+
+		try
+		{
+			var data = fs.readFileSync( _JSON_PATH + "/" + paramsO.brandNm + "/json/" + paramsO.brandNm + ".json" ).toString();
+		}
+		catch( err )
+		{
+			console.log( routerNm + " - DBJS File Not Found! - " + err );
+			res.end("{ sucess : 0, data : null }");
+		}
+
+		
+		var query = _tQuery.replace( "<!=BRAND_NM=!>", paramsO.brandNm )
+				.replace( "<!=DATA=!>", data );
+		var dbjs_nm = "insert_" + paramsO.brandNm + ".dbjs";
+
+		var FILE_PATH = DBJS_DIRECTORY_PATH + dbjs_nm;
+		
+		console.log( FILE_PATH )
+
+		fs.writeFileSync( DBJS_DIRECTORY_PATH + dbjs_nm , query, { flag : "w" } );
+
+		var r = exec_query_DB( dbjs_nm )
+		res.end( r )	
+
+	});
 })();
 
 //-------------------------------------------------------;
