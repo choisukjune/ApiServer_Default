a1e86935217be023a89b93b5a2970fbe84253bfa
choisukjune <sukjune.choi@gmail.com>
2023-07-05 18:24:44 +0900

    _oContentTypes add

--- index.js
+++ index.js
@@ -103,13 +103,64 @@ global.server = http.createServer(function(req, res){
 		var extname = path.extname(filePath);
 		
 		var _oContentTypes = {
-			'.html' : "text/html"
-			, '.js' : 'text/javascript'
-			, '.css' : 'text/css'
-			, '.json' : 'application/json'
-			, '.png' : 'image/png'
-			, '.jpg' : 'image/jpg'
-			, '.wav' : 'audio/wav'
+			".aac": "audio/aac",
+			".abw": "application/x-abiword",
+			".arc": "application/octet-stream",
+			".avi": "video/x-msvideo",
+			".azw": "application/vnd.amazon.ebook",
+			".bin": "application/octet-stream",
+			".bz": "application/x-bzip",
+			".bz2": "application/x-bzip2",
+			".csh": "application/x-csh",
+			".css": "text/css",
+			".csv": "text/csv",
+			".doc": "application/msword",
+			".epub": "application/epub+zip",
+			".gif": "image/gif",
+			".html": "text/html",
+			".htm": "text/html",
+			".ico": "image/x-icon",
+			".ics": "text/calendar",
+			".jar": "application/java-archive",
+			".jpeg.jpg": "image/jpeg",
+			".jpg.jpg": "image/jpeg",
+			".js": "application/js",
+			".json": "application/json",
+			".midi": "audio/midi",
+			".mid": "audio/midi",
+			".mpeg": "video/mpeg",
+			".mpkg": "application/vnd.apple.installer+xml",
+			".odp": "application/vnd.oasis.opendocument.presentation",
+			".ods": "application/vnd.oasis.opendocument.spreadsheet",
+			".odt": "application/vnd.oasis.opendocument.text",
+			".oga": "audio/ogg",
+			".ogv": "video/ogg",
+			".ogx": "application/ogg",
+			".pdf": "application/pdf",
+			".ppt": "application/vnd.ms-powerpoint",
+			".rar": "application/x-rar-compressed",
+			".rtf": "application/rtf",
+			".sh": "application/x-sh",
+			".svg": "image/svg+xml",
+			".swf": "application/x-shockwave-flash",
+			".tar": "application/x-tar",
+			".tiff": "image/tiff",
+			".tif": "image/tiff",
+			".ttf": "application/x-font-ttf",
+			".vsd": "application/vnd.visio",
+			".wav": "audio/x-wav",
+			".weba": "audio/webm",
+			".webm": "video/webm",
+			".webp": "image/webp",
+			".woff": "application/x-font-woff",
+			".xhtml": "application/xhtml+xml",
+			".xls": "application/vnd.ms-excel",
+			".xml": "application/xml",
+			".xul": "application/vnd.mozilla.xul+xml",
+			".zip": "application/zip",
+		//    ".3gp": "video/3gpp\naudio/3gpp if it doesn't contain video",
+		//    ".3g2": "video/3gpp2\naudio/3gpp2 if it doesn't contain video",
+			".7z": "application/x-7z-compressed"
 		};
 		var contentType = _oContentTypes[ extname ];
 		
